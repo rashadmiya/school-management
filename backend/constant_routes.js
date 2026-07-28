4f5c2a49af5b4d057fb8b2846d54b3ba0772cbec
rashadmiya <rashadmiya@gmail.com>
2026-07-28 20:40:28 +0600

    changes IP address again

--- backend/constant_routes.js
+++ backend/constant_routes.js
@@ -9,9 +9,9 @@
 //for production
 
 module.exports = {
-    activation_link: "http://192.168.0.36/activation/",
-    frontent: "http://192.168.0.36/",
-    backend: "http://192.168.0.36/",
+    activation_link: "http://192.168.11.11/activation/",
+    frontent: "http://192.168.11.11/",
+    backend: "http://192.168.11.11/",
 
     // FRONTEND_URL=http://192.168.0.36
     // BACKEND_URL=http://192.168.0.36
