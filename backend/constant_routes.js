19be0c6718b09c5d2b5ad28ac7f3921d102fdeb1
rashadmiya <rashadmiya@gmail.com>
2026-07-28 17:08:08 +0600

    constant route has updated

--- backend/constant_routes.js
+++ backend/constant_routes.js
@@ -1,15 +1,19 @@
 
 //for local
-module.exports = {
-    activation_link: "http://localhost:5173/activation/",
-    frontent: "http://localhost:5173/",
-    backend: "http://localhost:8000/",
-}
+// module.exports = {
+//     activation_link: "http://localhost:5173/activation/",
+//     frontent: "http://localhost:5173/",
+//     backend: "http://localhost:8000/",
+// }
 
 //for production
 
-// module.exports = {
-//     activation_link: "https://twirlnavigator.com/activation/",
-//     frontent: "https://twirlnavigator.com/",
-//     backend: "https://testapi.appayansoft.agency/",
-// }
+module.exports = {
+    activation_link: "http://192.168.0.36/activation/",
+    frontent: "http://192.168.0.36/",
+    backend: "http://192.168.0.36/",
+
+    // FRONTEND_URL=http://192.168.0.36
+    // BACKEND_URL=http://192.168.0.36
+    // ACTIVATION_URL=http://192.168.0.36/activation
+}
