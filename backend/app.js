dc037e226486b4fc08ed3c9e6fe365f92375c61e
rashadmiya <rashadmiya@gmail.com>
2026-07-28 16:31:44 +0600

    make the cors origin public in the app.js for test

--- backend/app.js
+++ backend/app.js
@@ -9,31 +9,36 @@ const app = express();
 
 
 // working cors
-const allowedOrigins = process.env.FRONTEND_URL
-  .split(",")
-  .map(origin => origin.trim());
+// const allowedOrigins = process.env.FRONTEND_URL
+//   .split(",")
+//   .map(origin => origin.trim());
 
-const corsOptions = {
-  origin(origin, callback) {
+// const corsOptions = {
+//   origin(origin, callback) {
 
-    // Allow requests like Postman or server-to-server
-    if (!origin) {
-      return callback(null, true);
-    }
+//     // Allow requests like Postman or server-to-server
+//     if (!origin) {
+//       return callback(null, true);
+//     }
 
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
+//     if (allowedOrigins.includes(origin)) {
+//       return callback(null, true);
+//     }
 
-    return callback(new Error("Not allowed by CORS"));
-  },
+//     return callback(new Error("Not allowed by CORS"));
+//   },
 
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// };
+
+// app.use(cors(corsOptions));
+
+app.use(cors({
+  origin: true,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
